a3fe1c9479c63c1063ffea9b0227014abc3cd3d3
Olivier DEWALQUE <olivier.dewalque@student.vinci.be>
2020-11-01 14:25:03 +0100

    test_CalculateMinutes_give12h00_returnNothing

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,4 +3,14 @@ namespace BerlinClock;
 
 class BerlinClock{
 
+    public function calculateMinutes($timer){
+        $var = explode(":", $timer);
+        $minutes = strval($var[1]);
+        $mod = $minutes%5;
+        $string = "";
+        for($i = 0; $i < $mod; $i++){
+            $string .= "Y";
+        }
+        return $string;
+    }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -5,7 +5,13 @@ require "BerlinClock.php";
 use BerlinClock\BerlinClock;
 use PHPUnit\Framework\TestCase;
 
-class BerlinClockTest extends TestCase
-{
+class BerlinClockTest extends TestCase {
+
+    public function test_CalculateMinutes_give12h00_returnNothing(){
+        $test = new BerlinClock();
+        $timer = "12:00:00";
+        $actual = $test->calculateMinutes($timer);
+        $this->assertEquals("",$actual);
+    }
 
 }
