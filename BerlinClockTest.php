e13fb25ab92f7d35af17c44cad193be828260ce3
Dalil RACHIK <dalil.rachik@student.vinci.be>
2020-11-01 15:24:13 +0100

    First Test Minute Two Y

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -15,12 +15,20 @@ class BerlinClockTest extends TestCase {
 
     }
 
-    public function test_CalculateMinutes_give12h01_oneY(){
+    public function test_CalculateMinutes_give12h01_returnOneY(){
         $test = new BerlinClock();
         $timer = "12:01:00";
         $actual = $test->calculateMinutes($timer);
         $this->assertEquals("Y",$actual);
     }
+
+    public function test_CalculateMinutes_give12h02_returnTwoY(){
+        $test = new BerlinClock();
+        $timer = "12:02:00";
+        $actual = $test->calculateMinutes($timer);
+        $this->assertEquals("YY",$actual);
+    }
+
 
 
 
