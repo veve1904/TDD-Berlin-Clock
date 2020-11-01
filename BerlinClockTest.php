08caf7ffb7f74bd07acfba732ed55f1c79ad6ce8
Dalil RACHIK <dalil.rachik@student.vinci.be>
2020-11-01 16:26:25 +0100

    Test Minutes By 5 return Nothing

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -121,6 +121,12 @@ class BerlinClockTest extends TestCase {
         $actual = $test->calculateMinutesBy5($timer);
         $this->assertEquals("YYRYYRYYRYY",$actual);
     }
+    public function test_CalculateMinutesBy5_give13h00_returnNothing(){
+        $test = new BerlinClock();
+        $timer = "13:00:00";
+        $actual = $test->calculateMinutesBy5($timer);
+        $this->assertEquals("",$actual);
+    }
 
 
 
